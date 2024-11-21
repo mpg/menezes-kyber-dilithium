bfa21ca46119788e60f2d4f023c57e2b2da0fa6e
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2024-11-21 15:59:25 +0100

    Implement the 3 sizes of Kyber-PKE (not just 768)
    Review with --ignore-space-change

--- kyber.py
+++ kyber.py
@@ -14,72 +14,84 @@ For a version closer to the lectures/slides, see previous commits.
 from kyber_math import KModPol, KVec, KMat
 from kyber_sym import G, PRF
 
-# Kyber-768
-k = 3
-eta1 = 2
-eta2 = 2
-du = 10
-dv = 4
 
+class KyberPKE:
+    """A simplified version (no NTT) of Kyber-PKE."""
 
-def genkey(d):
-    """Kyber-PKE key generation."""
-    # Algorithm 13 K-PKE.KeyGen
+    def __init__(self, param_set):
+        """Initialize the Kyber PKE with the given parameter set."""
+        # Table 2 Approved parameter sets for ML-KEM, p. 39
+        if param_set == 512:
+            self.k = 2
+            self.eta1, self.eta2 = 3, 2
+            self.du, self.dv = 10, 4
+        elif param_set == 768:
+            self.k = 3
+            self.eta1, self.eta2 = 2, 2
+            self.du, self.dv = 10, 4
+        elif param_set == 1024:
+            self.k = 4
+            self.eta1, self.eta2 = 2, 2
+            self.du, self.dv = 11, 5
+        else:
+            raise ValueError
 
-    rho, sigma = G(d + k.to_bytes(1))
-    prf = PRF(sigma)
+    def genkey(self, d):
+        """Kyber-PKE key generation."""
+        # Algorithm 13 K-PKE.KeyGen
 
-    A = KMat.uni_from_seed(k, rho)
+        rho, sigma = G(d + self.k.to_bytes(1))
+        prf = PRF(sigma)
 
-    s = KVec.cbd_from_prf(k, eta1, prf)
-    e = KVec.cbd_from_prf(k, eta2, prf)
+        A = KMat.uni_from_seed(self.k, rho)
 
-    t = A @ s + e
+        s = KVec.cbd_from_prf(self.k, self.eta1, prf)
+        e = KVec.cbd_from_prf(self.k, self.eta2, prf)
 
-    ek_pke = t.to_bytes() + rho
-    dk_pke = s.to_bytes()
+        t = A @ s + e
 
-    return (ek_pke, dk_pke)
+        ek_pke = t.to_bytes() + rho
+        dk_pke = s.to_bytes()
 
+        return (ek_pke, dk_pke)
 
-def encrypt(ek_pke, m, r):
-    """Kyber-PKE encryption."""
-    # Algorithm 14 K-PKE.Encrypt
+    def encrypt(self, ek_pke, m, r):
+        """Kyber-PKE encryption."""
+        # Algorithm 14 K-PKE.Encrypt
 
-    t = KVec.from_bytes(ek_pke[: 384 * k])
-    rho = ek_pke[384 * k :]
+        t = KVec.from_bytes(ek_pke[: 384 * self.k])
+        rho = ek_pke[384 * self.k :]
 
-    A = KMat.uni_from_seed(k, rho)
+        A = KMat.uni_from_seed(self.k, rho)
 
-    prf = PRF(r)
-    r = KVec.cbd_from_prf(k, eta1, prf)
-    e1 = KVec.cbd_from_prf(k, eta2, prf)
-    e2 = KModPol.cbd_from_prf(eta2, prf)
+        prf = PRF(r)
+        r = KVec.cbd_from_prf(self.k, self.eta1, prf)
+        e1 = KVec.cbd_from_prf(self.k, self.eta2, prf)
+        e2 = KModPol.cbd_from_prf(self.eta2, prf)
 
-    mu = KModPol.decompress_from_bytes(1, m)
+        mu = KModPol.decompress_from_bytes(1, m)
 
-    u = A.transpose() @ r + e1
-    v = t * r + e2 + mu
+        u = A.transpose() @ r + e1
+        v = t * r + e2 + mu
 
-    c1 = u.compress_to_bytes(du)
-    c2 = v.compress_to_bytes(dv)
+        c1 = u.compress_to_bytes(self.du)
+        c2 = v.compress_to_bytes(self.dv)
 
-    return c1 + c2
+        return c1 + c2
 
+    def decrypt(self, dk_pke, c):
+        """Kyber-PKE decryption."""
+        # Algorithm 15 K-PKE.Decrypt
 
-def decrypt(dk_pke, c):
-    """Kyber-PKE decryption."""
-    # Algorithm 15 K-PKE.Decrypt
+        c1 = c[: 32 * self.du * self.k]
+        c2 = c[32 * self.du * self.k :]
 
-    c1 = c[: 32 * du * k]
-    c2 = c[32 * du * k :]
+        ud = KVec.decompress_from_bytes(self.du, c1)
+        vd = KModPol.decompress_from_bytes(self.dv, c2)
 
-    ud = KVec.decompress_from_bytes(du, c1)
-    vd = KModPol.decompress_from_bytes(dv, c2)
+        s = KVec.from_bytes(dk_pke)
 
-    s = KVec.from_bytes(dk_pke)
+        w = vd - s * ud
+        m = w.compress_to_bytes(1)
 
-    w = vd - s * ud
-    m = w.compress_to_bytes(1)
-
-    return m
+        return m
--- kyber_test.py
+++ kyber_test.py
@@ -2,16 +2,20 @@ import unittest
 
 import secrets
 
-from kyber import genkey, encrypt, decrypt
+from kyber import KyberPKE
 
 
-class kyber_pke_test(unittest.TestCase):
-    def test_gen_enc_dec(self):
+class KyberPKETest(unittest.TestCase):
+    def test_rand_gen_enc_dec(self):
         d = secrets.token_bytes(32)
         r = secrets.token_bytes(32)
         msg = secrets.token_bytes(32)
 
-        pub, prv = genkey(d)
-        ct = encrypt(pub, msg, r)
-        dec = decrypt(prv, ct)
-        self.assertEqual(dec, msg)
+        for s in 512, 768, 1024:
+            pke = KyberPKE(s)
+
+            pub, prv = pke.genkey(d)
+            ct = pke.encrypt(pub, msg, r)
+            dec = pke.decrypt(prv, ct)
+
+            self.assertEqual(dec, msg)
