70a48a5a0cfc0648ebbf7fd81c68a84d2c201210
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2024-11-21 16:16:12 +0100

    Check sizes of outputs (end "from V2c to the spec")
    That is, the spec without the NTT, until V4b.

--- kyber_test.py
+++ kyber_test.py
@@ -6,10 +6,23 @@ from kyber import KyberPKE
 
 
 class KyberPKETest(unittest.TestCase):
-    def test_rand_gen_enc_dec(self):
+    def test_rand_gen_enc_dec_and_sizes(self):
         d = secrets.token_bytes(32)
         r = secrets.token_bytes(32)
         msg = secrets.token_bytes(32)
+
+        # Size -> encryption key len, decryption key len, ciphertext len,
+        #
+        # For the first and last colum (encryption key, ciphertext), use
+        # Table 3 Sizes (in bytes) of keys and ciphertexts of ML-KEM, p. 39.
+        #
+        # But encryption key (PKE) differs from decapsulation key (KEM).
+        # So here use 384 * k (dk_PKE input of Algorithm 15 K-PKE.Decrypt).
+        sizes = {
+            512: (800, 384 * 2, 768),
+            768: (1184, 384 * 3, 1088),
+            1024: (1568, 384 * 4, 1568),
+        }
 
         for s in 512, 768, 1024:
             pke = KyberPKE(s)
@@ -19,3 +32,8 @@ class KyberPKETest(unittest.TestCase):
             dec = pke.decrypt(prv, ct)
 
             self.assertEqual(dec, msg)
+
+            ek_len, dk_len, ct_len = sizes[s]
+            self.assertEqual(len(pub), ek_len)
+            self.assertEqual(len(prv), dk_len)
+            self.assertEqual(len(ct), ct_len)
